c3d55089bf4ad5126db0a1c5e4add86a32e28e2f
estefany-rm <estefany18.ramirez@gmail.com>
2021-11-28 00:11:54 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/estefany-rm/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        {""} by Estefany Ramirez
+      </footer>
+    </div>
+  );
 }
